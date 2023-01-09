120b304db19986c9e64602ed2ddc391d305c166e
konstantinnovation <konstans@stuy.edu>
2023-01-09 12:11:14 -0500

    Create Recursion.java

---
+++ Recursion.java
@@ -0,0 +1,40 @@
+public class Recursion{
+  
+   /*Print all words that are made of the letters a-e inclusive.
+    *@param length : the length of the words that are to be printed
+    */
+    public static void printAllWords(int length){
+      //do not change this method.
+      printAllWords(length,"");
+    }
+
+   /*Print all words that are made of the letters a-e inclusive.
+    *@param length : either how many more letters or the total length depending on implementation
+    *@param word   : the partial word so far.
+    */
+    public static void printAllWords(int length,String word){
+      //WRITE THIS METHOD
+    }
+  
+  /*Print all words that are made of the characters in the array of letters.
+    *There may not be consecutive equal letters, so:
+    *aax is not allowed, but axa is allowed.
+    *@param length : the length of the words that are to be printed
+    *@param letters: the letters you should be using
+    *@precondition: letters contains at least 2 characters, and has no duplicates.
+    */
+    public static void printNoDoubleLetterWords(int length,char[] letters){
+      //do not change this method
+      printNoDoubleLetterWords(length,"",letters);
+    }
+
+    /*Print all words that are made of the characters in letters. There may not be consecutive equal letters,
+    *aax is not allowed, but axa is allowed.
+    *@param length : either how many more letters need to be
+    *@param word   : the partial word so far.
+    *@param letters: the letters you should be using
+    */
+    public static void printNoDoubleLetterWords(int length,String word,char[]letters){
+      //WRITE THIS METHOD
+    }
+}
