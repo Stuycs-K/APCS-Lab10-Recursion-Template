cd30efb646ef79d390e79364a9c38e6bddc11568
konstantinnovation <konstans@stuy.edu>
2024-01-05 09:12:34 -0500

    Update Recursion.java

--- Recursion.java
+++ Recursion.java
@@ -1,20 +1,5 @@
 public class Recursion{
   
-   /*Print all words that are made of the letters a-e inclusive.
-    *@param length : the length of the words that are to be printed
-    */
-    public static void printAllWords(int length){
-      //do not change this method.
-      printAllWords(length,"");
-    }
-
-   /*Print all words that are made of the letters a-e inclusive.
-    *@param length : either how many more letters or the total length depending on implementation
-    *@param word   : the partial word so far.
-    */
-    public static void printAllWords(int length,String word){
-      //WRITE THIS METHOD
-    }
   
   /*Print all words that are made of the characters in the array of letters.
     *There may not be consecutive equal letters, so:
@@ -23,7 +8,7 @@ public class Recursion{
     *@param letters: the letters you should be using
     *@precondition: letters contains at least 2 characters, and has no duplicates.
     */
-    public static void printNoDoubleLetterWords(int length,char[] letters){
+    public static void printNoDoubleLetterWords(int length,String letters){
       //do not change this method
       printNoDoubleLetterWords(length,"",letters);
     }
@@ -34,7 +19,17 @@ public class Recursion{
     *@param word   : the partial word so far.
     *@param letters: the letters you should be using
     */
-    public static void printNoDoubleLetterWords(int length,String word,char[]letters){
+    public static void printNoDoubleLetterWords(int length,String word, String letters){
       //WRITE THIS METHOD
+    }
+
+    /*Convert the integer to a String containing English words that are used to say the number.
+    * toWords(0) returns "zero"
+    * toWords(340) returns "three hundred and forty"
+    * toWords(1000430) returns: "one million four hundred and thirty"
+    * toWords(-2101000442) returns: "negative two billion one hundred one million four hundred and forty-two"
+    */
+    public static String toWords(int n){
+
     }
 }
